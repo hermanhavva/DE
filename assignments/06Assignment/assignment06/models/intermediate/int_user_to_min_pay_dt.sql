2ab51981a351a123760c5a06d97d1676289ebfa7
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2025-04-16 19:59:04 +0300

    add stg models

--- assignments/06Assignment/assignment06/models/intermediate/int_user_to_min_pay_dt.sql
+++ assignments/06Assignment/assignment06/models/intermediate/int_user_to_min_pay_dt.sql
@@ -1,8 +1,8 @@
 -- /models/intermediate/int_user_to_min_pay_dt.sql
 
 select
-  p.user_id,
-  get_min_max_value(pay_dt).min_value as min_pay_dt
+  distinct p.user_id,
+  min(p.pay_dt) over (partition by p.user_id) as min_pay_dt
 from
-  {{ref('stg_payments')}} p
-  group by p.user_id)
+  {{ ref('stg_payments') }} p
+
--- assignments/06Assignment/assignment06/models/marts/mart_avg_ses_count_before_and_after_first_pay.sql
+++ assignments/06Assignment/assignment06/models/marts/mart_avg_ses_count_before_and_after_first_pay.sql
@@ -1,37 +1,5 @@
 -- models/marts/mart_avg_sessions_before_after_pay.sql
 
---with ses_per_user_before as (
---    select
---        s.user_id,
---        count(distinct s.session_number) as ses_count
---    from {{ ref('stg_sessions') }} s
---    inner join {{ ref('stg_payments') }} p
---        on p.user_id = s.user_id
---    inner join {{ ref('stg_registrations') }} r
---        on r.user_id = s.user_id
---    inner join {{ ref('int_user_to_min_pay') }} utmp
---        on utmp.user_id = s.user_id
---    where s.session_start_dt < utmp.min_pay_dt
---      and r.country_code = 'UA'
---    group by s.user_id
---),
---
---ses_per_user_after as (
---    select
---        s.user_id,
---        count(distinct s.session_number) as ses_count
---    from {{ ref('stg_sessions') }} s
---    inner join {{ ref('stg_payments') }} p
---        on p.user_id = s.user_id
---    inner join {{ ref('stg_registrations') }} r
---        on r.user_id = s.user_id
---    inner join {{ ref('int_user_to_min_pay') }} mpdpu
---        on mpdpu.user_id = s.user_id
---    where s.session_start_dt >= mpdpu.min_dt
---      and r.country_code = 'UA'
---    group by s.user_id
---)
-
 select
     (select avg(ses_count) from {{ref('int_ses_per_user_before_pay')}}) as before_pay_avg_session,
     (select avg(ses_count) from {{ref('int_ses_per_user_after_first_pay')}}) as after_pay_avg_session
